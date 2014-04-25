d36fd4f1939d0cec664510c6108605778134bbbc
Aaron Nech <aaron@localhost.localdomain>
2014-04-25 11:03:11 -0700

    fixed

--- public/js/ui.js
+++ public/js/ui.js
@@ -209,56 +209,28 @@ function UIViewModel() {
 	});
 
 	if(Leap) {
-		// Store frame for motion functions
-		var previousFrame = null;
+		var controller = new Leap.Controller({enableGestures: true});
 
-		// Setup Leap loop with frame callback function
-		var controllerOptions = {enableGestures: true};
-
-		Leap.loop(controllerOptions, function(frame) {
-
-		  // Display Gesture object data
-		  var gestureOutput = document.getElementById("gestureData");
-		  var gestureString = "";
-		  if (frame.gestures.length > 0) {
-		    for (var i = 0; i < frame.gestures.length; i++) {
-		      var gesture = frame.gestures[i];
-
-		      switch (gesture.type) {
-		        case "swipe":
-		          //Classify swipe as either horizontal or vertical
-		          var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
-		          //Classify as right-left or up-down
-		          if(isHorizontal){
-		              if(gesture.direction[0] > 0){
-		                  swipeDirection = "right";
-		              } else {
-		                  swipeDirection = "left";
-		              }
-		          } else { //vertical
-		              if(gesture.direction[1] > 0){
-		                  swipeDirection = "up";
-		              } else {
-		                  swipeDirection = "down";
-		              }                  
-		          }
-		          console.log(swipeDirection);
-		          break;
-		       }
-		     }
-		  }
-		  
-
+		controller.on('gesture', function (gesture){
+		    if(gesture.type === 'swipe'){
+		        handleSwipe(gesture);
+		    }
 		});
 
-		function vectorToString(vector, digits) {
-		  if (typeof digits === "undefined") {
-		    digits = 1;
-		  }
-		  return "(" + vector[0].toFixed(digits) + ", "
-		             + vector[1].toFixed(digits) + ", "
-		             + vector[2].toFixed(digits) + ")";
+		function handleSwipe (swipe){
+		    var startFrameID;
+		    if(swipe.state === 'stop'){
+		        if (swipe.direction[0] > 0){
+		            //this means that the swipe is to the right direction
+		            moveRight();
+		        }else{
+		            //this means that the swipe is to the left direction
+		            moveLeft();
+		        }
+		    }
 		}
+
+		controller.connect();
 	}
 
 	/**
