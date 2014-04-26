758e6d5dd387cb64331e9ea79d0bd2609a6e3ef5
Karan Goel <karan@goel.im>
2014-04-26 00:55:05 -0700

    basic sc done

--- public/js/play.js
+++ public/js/play.js
@@ -1,3 +1,35 @@
 $(function () {
-  console.log('playing song');
+
+  // initUI();
+
+  var client_id = '2aaf60470a34d42b0561e92b17ec7ce2';
+  var currentQueue = []; // holds the queue of songs
+  var audioElem = $("#widget")[0];
+
+  // initialize SC add
+  SC.initialize({
+    client_id: client_id
+  });
+
+  // on page load, get and play something
+  playNext();
+
+
+  function playNext() {
+    // update the audio tag source
+    if (currentQueue.length == 0) {
+      // empty queue, get more songs
+      $.get( "/nextsong", function(data) {
+        currentQueue = data.tracks;
+        console.log(currentQueue);
+        // play the first song
+        var url = currentQueue.pop().songUrl;
+        console.log(url);
+        SC.get(url, {}, function(sound, error) {
+          $('#widget').attr('src', sound.stream_url + '?client_id=' + client_id);
+        });
+      });
+    }
+  }
+
 });
