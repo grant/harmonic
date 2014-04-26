87d1878638d2a8aad93a2118283a79d1b19e58ee
Ryan Drapeau <drapeau@cs.washington.edu>
2014-04-25 20:57:57 -0700

    Main Page

---
+++ frontend/js/index.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+
+    $(document).ready(function() {
+        $("#fb").hide().delay(200).fadeIn("slow");
+        $("#fb").click(login);
+    });
+
+    var login = function() {
+        // Login Stuff goes here
+    };
+})();
