a12cb8c1c55a65eea0aa4f09c5c632978546ce70
Ryan Drapeau <drapeau@cs.washington.edu>
2014-04-25 21:08:10 -0700

    Button animation

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,6 +4,14 @@
     $(document).ready(function() {
         $("#fb").hide().delay(200).fadeIn("slow");
         $("#fb").click(login);
+        $("#fb").hover(
+            function() {
+                $(this).stop().animate({"opacity": "0.65"}, "slow");
+            },
+            function() {
+                $(this).stop().animate({"opacity": "1"}, "slow");
+            }
+        );
     });
 
     var login = function() {
