1b3cbc40f64b5017bd4538e7ba27ba4c6253c8ad
Ryan Drapeau <drapeau@cs.washington.edu>
2014-04-25 22:12:15 -0700

    page

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -12,7 +12,27 @@
                 $(this).stop().animate({"opacity": "1"}, "slow");
             }
         );
+
+        $(".song-name").hide();
+        $(".bar-item").hover(
+            function() {
+                $(this).children().stop();
+                $(this).find(".album-photo").animate({
+                    bottom: "120px"
+                }, 700, function() {
+                    $(this).parent().find(".song-name").fadeIn(300);
+                });
+            },
+            function() {
+                $(this).children().stop();
+                $(this).find(".song-name").fadeOut(300);
+                $(this).find(".album-photo").animate({
+                    bottom: "35px"
+                }, 700);
+            }
+        );
     });
+
 
     var login = function() {
         // Login Stuff goes here
