1b987525380db26ea4e416caa6a328d18729d5cc
Grant Timmerman <granttimmerman@gmail.com>
2014-04-25 20:23:57 -0700

    Added play page frontend

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function (req, res){
   res.render('index', { title: 'Harmonic' });
+};
+
+exports.play = function (req, res) {
+  res.render('play', { title: 'Play -- Harmonic' });
 };
