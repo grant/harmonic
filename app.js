5ad31b32c81ca020c17c51277f3dda4c2634861e
Grant Timmerman <granttimmerman@gmail.com>
2014-04-25 20:03:12 -0700

    Created base framework for frontend

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -29,7 +28,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
