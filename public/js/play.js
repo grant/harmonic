0ec649267c6ca02bb0d1114091dfc47d603859a6
Grant Timmerman <granttimmerman@gmail.com>
2014-04-26 02:18:18 -0700

    Fixed profile photo css

--- public/js/play.js
+++ public/js/play.js
@@ -43,7 +43,6 @@ $(function () {
   function playOne(url) {
     SC.get(url, {}, function(sound, error) {
       if (sound.stream_url) {
-        console.log(sound);
         $('#widget').attr('src', sound.stream_url + '?client_id=' + client_id);
         if (sound.artwork_url) {
           var img = $('<img/>').attr('src', sound.artwork_url.replace("large", "crop"));
